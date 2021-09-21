7c0409472bfb251849b7bb2d456aa35b867ff06d
vinita <vinitapoonia333@gmail.com>
2021-09-22 01:39:04 +0530

    articles app created , visibility of articles on website , user authorization

---
+++ articles/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+from django.conf import settings
+
+from django.contrib.auth import get_user_model
+
+from django.urls import reverse
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length= 500)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    img_field_name = models.ImageField(upload_to="images/", height_field=None, width_field=None, max_length=100, null = True , blank= True )
+
+    file_field_name = models.FileField(upload_to="images/", max_length=254, null= True, blank=True)
+
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete = models.CASCADE,
+    )
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article_detail' , args = [str(self.id)])
--- newspaper/settings.py
+++ newspaper/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'articles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -122,6 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
